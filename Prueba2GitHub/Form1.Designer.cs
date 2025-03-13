bffbab02e8a6552b658bceb961357334231a5f6c
Oscar Villa claudio <oscar658villa@gmail.com>
2025-03-13 08:59:08 -0600

    cambio color

--- Prueba2GitHub/Form1.Designer.cs
+++ Prueba2GitHub/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pictureBox1.BackColor = System.Drawing.Color.OrangeRed;
             this.pictureBox1.Location = new System.Drawing.Point(-5, 43);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(806, 52);
